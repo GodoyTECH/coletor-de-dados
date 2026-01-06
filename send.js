9da616545cdac27cc3f836832baf9c9c659c24d3
Caique Eduardo <caiqueworkti@gmail.com>
2026-01-06 16:00:17 -0300

    Update send.js

--- send.js
+++ send.js
@@ -13,9 +13,9 @@
    ================================ */
 
 const CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw3TPvSFOlTE9E-sTOMsLaSCvzx1_tFM_BqGUmddn_ugI3qfQ87JxIoSW0wArKzKoCM/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxw-FYkrlqXXmtM_-PgKUYdxMA7oBb9xeRg_cUiw02h7Fopv-QQ4uBLYDS55Xd7m0pY/exec",
 
-  SYSTEM_URL: "https://script.google.com/macros/s/AKfycbw3TPvSFOlTE9E-sTOMsLaSCvzx1_tFM_BqGUmddn_ugI3qfQ87JxIoSW0wArKzKoCM/exec",
+  SYSTEM_URL: "https://script.google.com/macros/s/AKfycbxw-FYkrlqXXmtM_-PgKUYdxMA7oBb9xeRg_cUiw02h7Fopv-QQ4uBLYDS55Xd7m0pY/exec",
 
   GOOGLE_SHEETS_URL: "https://docs.google.com/spreadsheets/d/1zgpcoY7oh2Hm_mQyj5Ev6jyBtxQWYrh0ozad1w8-a2g/edit",
 
